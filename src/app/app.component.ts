a2d6194e3dde099a2929dbd2d8dac3bdb4791711
hanan11240 <peerzadahanannaseer@gmail.com>
2025-08-31 15:52:52 +0530

    make test api call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -8,5 +9,21 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-project-deployment';
+    configData: any;
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    const clientId = 'YOUR_CLIENT_ID'; // replace with actual client id
+    const url = `https://testassessment.educian.com/auth/get-configuration?client_id=${clientId}`;
+
+    this.http.get(url).subscribe({
+      next: (res) => {
+        console.log('API Response:', res);
+        this.configData = res;
+      },
+      error: (err) => {
+        console.error('API Error:', err);
+      }
+    });
+  }
 }
